981b2ad80714820195204ef2aebfd114c43b7dab
tony <toonyrenner@gmail.com>
2023-03-18 16:28:13 -0300

    adding Dependency and Setting ModelMapper.

--- src/main/java/com/github/itstoony/mercearia/MerceariaApplication.java
+++ src/main/java/com/github/itstoony/mercearia/MerceariaApplication.java
@@ -1,10 +1,17 @@
 package com.github.itstoony.mercearia;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MerceariaApplication {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(MerceariaApplication.class, args);
