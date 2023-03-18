1456fad7395d8bd4805b8efa2c2eb2e99997806b
tony <toonyrenner@gmail.com>
2023-03-18 16:03:46 -0300

    creating project

---
+++ src/test/java/com/github/itstoony/mercearia/MerceariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.itstoony.mercearia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MerceariaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
