c6e77d2eb652775e348eac4e90e9ec1e1b701318
tony <toonyrenner@gmail.com>
2023-03-19 15:21:02 -0300

    implementing listAll on ProductService.

--- src/main/java/com/github/itstoony/mercearia/repository/ProductRepository.java
+++ src/main/java/com/github/itstoony/mercearia/repository/ProductRepository.java
@@ -1,8 +1,11 @@
 package com.github.itstoony.mercearia.repository;
 
 import com.github.itstoony.mercearia.model.Product.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
+    Page<Product> findByName(String name, Pageable pageable);
 }
