cdbec2976db7ca60e7f965210517117efc43dbe8
Tony <toonyrenner@gmail.com>
2023-04-30 07:32:09 -0300

    Adding ControllerAdvice, ApiErrors and BusinessException.

---
+++ src/main/java/itstoony/com/github/mercearia/exception/ApiErrors.java
@@ -0,0 +1,27 @@
+package itstoony.com.github.mercearia.exception;
+
+import org.springframework.validation.BindingResult;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class ApiErrors {
+
+    private final List<String> errors;
+
+    public ApiErrors(BindingResult bindingResult) {
+        this.errors = new ArrayList<>();
+        bindingResult.getAllErrors().forEach(e -> this.errors.add(e.getDefaultMessage()));
+    }
+
+    public ApiErrors(BusinessException ex) {
+        this.errors = Collections.singletonList(ex.getMessage());
+    }
+
+    public ApiErrors(ResponseStatusException ex) {
+        this.errors = Collections.singletonList(ex.getReason());
+    }
+
+}
